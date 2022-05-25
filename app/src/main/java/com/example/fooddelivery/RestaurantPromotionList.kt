4ebd76337efac8ee966bce716eaed2e009ad6bf3
Thế Vĩ <88361806+TheViHuynh@users.noreply.github.com>
2022-05-25 15:34:24 +0700

    update done orders, intent detail

--- app/src/main/java/com/example/fooddelivery/RestaurantPromotionList.kt
+++ app/src/main/java/com/example/fooddelivery/RestaurantPromotionList.kt
@@ -1,3 +1,31 @@
 package com.example.fooddelivery
 
-data class RestaurantPromotionList(var name : String, var expiry : String)
+import android.os.Parcel
+import android.os.Parcelable
+
+data class RestaurantPromotionList(var name : String, var expiry : String) : Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString()!!,
+        parcel.readString()!!
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(name)
+        parcel.writeString(expiry)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<RestaurantPromotionList> {
+        override fun createFromParcel(parcel: Parcel): RestaurantPromotionList {
+            return RestaurantPromotionList(parcel)
+        }
+
+        override fun newArray(size: Int): Array<RestaurantPromotionList?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
